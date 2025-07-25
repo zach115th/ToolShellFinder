3e47cc8f65ae4c07b6b69a23e961e1d32e270c36
zach115th <zach115th@users.noreply.github.com>
2025-07-25 13:35:31 -0700

    Update toolshellfinderPS7.ps1

--- toolshellfinderPS7.ps1
+++ toolshellfinderPS7.ps1
@@ -1,4 +1,4 @@
-$logRoot = "F:\C\inetpub\logs\LogFiles"
+$logRoot = "C:\inetpub\logs\LogFiles"
 
 # ---------- constants ----------
 $requiredFields = @(
@@ -6,13 +6,13 @@ $requiredFields = @(
     'cs-uri-query','cs(User-Agent)','cs(Referer)'
 )
 
-# IoC Set 1
+# IoC Set 1 ─ ToolPane abuse
 $method1   = 'POST'
 $uriStems1 = @('/_layouts/15/ToolPane.aspx','/_layouts/16/ToolPane.aspx')
 $uriQuery1 = 'DisplayMode=Edit&a=/ToolPane.aspx'
 $referer1  = '/_layouts/SignOut.aspx'
 
-# IoC Set 2
+# IoC Set 2 ─ suspicious file names
 $method2   = 'GET'
 $referer2  = '/_layouts/SignOut.aspx'
 $uriFilePatterns = @(
@@ -21,13 +21,12 @@ $uriFilePatterns = @(
 )
 $uriRegex2 = '^/_layouts/(15|16)/(' + ($uriFilePatterns -join '|').Replace('.','\.') + ')$'
 
-# IoC Set 3
-$method3   = 'GET'
-$uriStems3 = @('/_layouts/15/start.aspx','/_layouts/16/start.aspx')
+# IoC Set 3 ─ any *.aspx under /_layouts/15|16/ with naughty UA strings
+$method3          = 'GET'
+$uriWildcardRegex = '^/_layouts/(15|16)/[^/]+\.aspx$'
 $userAgentIndicators  = @('curl','powershell','python')
 
-# IoC Set 4
-$uriStems4            = @('/_layouts/15/success.aspx','/_layouts/16/success.aspx')
+# IoC Set 4 ─ same wildcard + big VIEWSTATE + naughty UA strings
 $userAgentIndicators4 = @('curl','powershell','python')
 $viewstateRegex       = '^__VIEWSTATE=.*'
 
@@ -37,31 +36,26 @@ $logFiles = Get-ChildItem -Path $logRoot -Recurse -Include *.log
 # ---------- parallel scan ----------
 $results = $logFiles | ForEach-Object -Parallel {
 
-    # the pipeline item is $_ ; capture it for clarity
-    $logFile   = $_
-    $filePath  = $logFile.FullName
-    $fieldIdx  = @{}
+    $logFile    = $_
+    $filePath   = $logFile.FullName
+    $fieldIdx   = @{}
     $headerSeen = $false
-    $hits = @()
+    $hits       = @()
 
-    #foreach ($raw in Get-Content -LiteralPath $filePath) 
     foreach ($raw in [System.IO.File]::ReadLines($filePath)) {
 
         $line = $raw.Trim()
 
         if ($line.StartsWith('#Fields:')) {
             $headerSeen = $true
-            $fieldIdx = @{}
-            $fields = $line.Substring(8).Trim().Split(' ')
+            $fieldIdx   = @{}
+            $fields     = $line.Substring(8).Trim().Split(' ')
             for ($i = 0; $i -lt $fields.Count; $i++) { $fieldIdx[$fields[$i]] = $i }
             continue
         }
 
         if (-not $headerSeen -or $line.StartsWith('#') -or [string]::IsNullOrWhiteSpace($line)) { continue }
-
-	if ($line.IndexOf('/_layouts') -lt 0) { continue }
-        # make sure the line has every field we care about
-        #if ( ($using:requiredFields | Where-Object { -not $fieldIdx.ContainsKey($_) }).Count ) { continue }
+        if ($line.IndexOf('/_layouts') -lt 0) { continue }
 
         $cols      = $line.Split(' ')
         $dateVal   = $cols[$fieldIdx['date']]
@@ -73,47 +67,63 @@ $results = $logFiles | ForEach-Object -Parallel {
         $refVal    = $cols[$fieldIdx['cs(Referer)']]
 
         switch ($true) {
+
             { $methodVal -eq $using:method1 -and
               $using:uriStems1 -contains $stemVal -and
               $queryVal -like "*$($using:uriQuery1)*" -and
               $refVal   -like "*$($using:referer1)*" } {
+
                 $hits += [pscustomobject]@{
-                    IoCType='ToolPane_POST'; File=$filePath
-                    Date=$dateVal; Time=$timeVal; Method=$methodVal
-                    UserAgent=$uaVal; UriStem=$stemVal; UriQuery=$queryVal; Referer=$refVal; Line=$line
+                    IoCType   = 'ToolPane_POST'
+                    File      = $filePath
+                    Date      = $dateVal; Time = $timeVal; Method = $methodVal
+                    UserAgent = $uaVal;   UriStem = $stemVal;   UriQuery = $queryVal
+                    Referer   = $refVal;  Line = $line
                 }
             }
+
             { $methodVal -eq $using:method2 -and
               $stemVal -match $using:uriRegex2 -and
               $refVal  -like "*$($using:referer2)*" } {
+
                 $hits += [pscustomobject]@{
-                    IoCType='Suspicious_GET'; File=$filePath
-                    Date=$dateVal; Time=$timeVal; Method=$methodVal
-                    UserAgent=$uaVal; UriStem=$stemVal; UriQuery=$queryVal; Referer=$refVal; Line=$line
+                    IoCType   = 'Suspicious_GET'
+                    File      = $filePath
+                    Date      = $dateVal; Time = $timeVal; Method = $methodVal
+                    UserAgent = $uaVal;   UriStem = $stemVal;   UriQuery = $queryVal
+                    Referer   = $refVal;  Line = $line
                 }
             }
+
             { $methodVal -eq $using:method3 -and
-              $using:uriStems3 -contains $stemVal -and
+              $stemVal   -match $using:uriWildcardRegex -and
               ($using:userAgentIndicators | Where-Object { $uaVal -like "*$_*" }) } {
+
                 $hits += [pscustomobject]@{
-                    IoCType='StartAspx_SuspiciousUA'; File=$filePath
-                    Date=$dateVal; Time=$timeVal; Method=$methodVal
-                    UserAgent=$uaVal; UriStem=$stemVal; UriQuery=$queryVal; Referer=$refVal; Line=$line
+                    IoCType   = 'LayoutsAspx_SuspiciousUA'
+                    File      = $filePath
+                    Date      = $dateVal; Time = $timeVal; Method = $methodVal
+                    UserAgent = $uaVal;   UriStem = $stemVal;   UriQuery = $queryVal
+                    Referer   = $refVal;  Line = $line
                 }
             }
-            { $using:uriStems4 -contains $stemVal -and
+
+            { $stemVal -match $using:uriWildcardRegex -and
               $queryVal -match $using:viewstateRegex -and
               ($using:userAgentIndicators4 | Where-Object { $uaVal -like "*$_*" }) } {
+
                 $hits += [pscustomobject]@{
-                    IoCType='SuccessAspx_ViewState_SuspiciousUA'; File=$filePath
-                    Date=$dateVal; Time=$timeVal; Method=$methodVal
-                    UserAgent=$uaVal; UriStem=$stemVal; UriQuery=$queryVal; Referer=$refVal; Line=$line
+                    IoCType   = 'LayoutsAspx_ViewState_SuspiciousUA'
+                    File      = $filePath
+                    Date      = $dateVal; Time = $timeVal; Method = $methodVal
+                    UserAgent = $uaVal;   UriStem = $stemVal;   UriQuery = $queryVal
+                    Referer   = $refVal;  Line = $line
                 }
             }
         }
     }
 
-    $hits   # emit matches from this log file
+    $hits   # emit matches for this file
 } -ThrottleLimit 12
 
 # ---------- output ----------
@@ -122,5 +132,5 @@ if ($results) {
     $results | Export-Csv -Path .\IIS_IoC_Matches.csv -NoTypeInformation
     Write-Host "`nMatches exported to IIS_IoC_Matches.csv`n"
 } else {
-    Write-Host 'No matches found.'
+    Write-Host 'No matches found.`n'
 }
