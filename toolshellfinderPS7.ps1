c537a2623f0eae82e4556b4ef7d4ace77d03a245
zach115th <zach115th@users.noreply.github.com>
2025-07-28 17:58:04 -0700

    Update toolshellfinderPS7.ps1

--- toolshellfinderPS7.ps1
+++ toolshellfinderPS7.ps1
@@ -22,7 +22,8 @@ $uriFilePatterns = @(
     '3plx\.aspx',
     'debug_dev\.js',
     'info\.js',
-    'spinstaller\.aspx'
+    'spinstaller\.aspx';
+    'machinekey\.aspx'
 )
 $uriRegex2 = '^/_layouts/(15|16)/(' + ($uriFilePatterns -join '|').Replace('.','\.') + ')$'
 
